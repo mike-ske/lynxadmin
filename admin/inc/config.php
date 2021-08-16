880b8cb45b3955f98f7535b796781c4d3b359254
mike-ske <66280654+mike-ske@users.noreply.github.com>
2021-08-17 00:58:43 +0100

    added new pages

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -1,17 +1,17 @@
 <?php
 
 // ========== REMOTE DATABASE CONNECTION =======
-// define('DB_HOST', 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-// define('DB_PASS', 'x3re6upu94v48m0x');
-// define('DB_NAME', 'r8qw8oyoc0awxtvs');
-// define('DB_USER', 'hx1qv76lul5chjhv');
+define('DB_HOST', 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_PASS', 'x3re6upu94v48m0x');
+define('DB_NAME', 'r8qw8oyoc0awxtvs');
+define('DB_USER', 'hx1qv76lul5chjhv');
 
 // ========== LOCAL DATABASE CONNECTION =======
-define('DB_HOST', 'localhost');
-define('DB_PASS', '');
-define('DB_NAME', 'lynx_db');
-define('DB_USER', 'root');
-define('BASE_URL', 'http://localhost/lynxsite/');
+// define('DB_HOST', 'localhost');
+// define('DB_PASS', '');
+// define('DB_NAME', 'lynx_db');
+// define('DB_USER', 'root');
+// define('BASE_URL', 'http://localhost/lynxsite/');
 
 
 // FUNCTION TO CONVERT DB DATE TO UNIX DATE AND TIMESTAMP
