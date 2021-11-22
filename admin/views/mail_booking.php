6ebfa97d5a60cc77d4bc9cc5037b9890ccf56f91
mike-ske <66280654+mike-ske@users.noreply.github.com>
2021-11-22 06:09:29 -0800

    Update mail_booking.php

--- admin/views/mail_booking.php
+++ admin/views/mail_booking.php
@@ -53,8 +53,8 @@ if (isset($_POST['send']))
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = "thinksoftcreative@gmail.com";                     // SMTP username
-        $mail->Password   = "thinksoft2021";                               // SMTP password
+        $mail->Username   = "lynxdna1@gmail.com";                     // SMTP username
+        $mail->Password   = "#lynx@2017#";                          // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
