29d0f32f5a42c1192f1f0c8dae3bce74415e1bc0
mike-ske <66280654+mike-ske@users.noreply.github.com>
2022-10-28 00:39:53 +0100

    Update add_post.php

--- admin/pages/add_post.php
+++ admin/pages/add_post.php
@@ -33,8 +33,9 @@
                         <label for="exampleInputFile">Input File</label>
                         <div class="input-group">
                             <div class="custom-file">
-                                <input type="file" name="image" class="custom-file-input" id="exampleInputFile">
-                                <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                <input type="file" name="image" class="custom-file-input" id="photo">
+                                <label id="profile" class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                 
                             </div>
                         </div>
                     </div>
